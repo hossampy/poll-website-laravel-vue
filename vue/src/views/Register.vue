daa7f3ec9d4ba6a16059dfc8d7a108957176c0ad
hossam <hossameddine.djennad@gmail.com>
2024-04-09 23:54:13 +0200

    Update  package.json file

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -59,12 +59,13 @@ export default {
         <div>
           <div class="flex items-center justify-between">
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-            <div class="text-sm">
-              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-            </div>
+
           </div>
           <div class="mt-2">
             <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+          <div class="text-sm mt-5">
+            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
           </div>
         </div>
 
@@ -76,7 +77,7 @@ export default {
       <p class="mt-10 text-center text-sm text-gray-500">
         are u a member?
         {{ '' }}
-        <a href="{{'login'}}" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">login now</a>
+        <router-link :to="{name:'Login'}" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">login now</router-link>
       </p>
     </div>
   </div>
--- vue/src/views/login.vue
+++ vue/src/views/login.vue
@@ -32,12 +32,13 @@ export default {
         <div>
           <div class="flex items-center justify-between">
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-            <div class="text-sm">
-              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-            </div>
+
           </div>
           <div class="mt-2">
             <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+          <div class="text-sm mt-5  ">
+            <a href="#" class="font-semibold text-red-700 hover:text-red-400">Forgot password?</a>
           </div>
         </div>
 
@@ -49,7 +50,8 @@ export default {
       <p class="mt-10 text-center text-sm text-gray-500">
         Not a member?
         {{ ' ' }}
-        <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+        <router-link :to="{name :'Register'}" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial
+        </router-link>
       </p>
     </div>
 
