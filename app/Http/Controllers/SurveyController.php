5c3ed70f9b53b580d37e171503f97b65c69c9676
hossam <hossameddine.djennad@gmail.com>
2024-04-14 02:29:11 +0200

    index surveycontroller

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\SurveyResource;
 use App\Models\Survey;
 use App\Http\Requests\storeSurveyRequest;
 use App\Http\Requests\UpdateSurveyRequest;
+use Illuminate\Http\Request;
 
 class SurveyController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $user = $request->user();
+
+        return SurveyResource::collection(Survey::where('user_id', $user->id)->orderBy('created_at', 'DESC')->paginate(10));
     }
 
     /**
