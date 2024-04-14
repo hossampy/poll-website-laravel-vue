5ad0c7d985489cf15c8d02e145fedfecba2ed76d
hossam <hossameddine.djennad@gmail.com>
2024-04-14 03:19:24 +0200

    survey request

--- app/Http/Requests/StoreSurveyRequest.php
+++ app/Http/Requests/StoreSurveyRequest.php
@@ -11,7 +11,16 @@ class StoreSurveyRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
+    }
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'user_id' => $this->user()->id
+
+            /* This ensures that when a new survey is created,
+             it is associated with the authenticated user who created it. */
+        ]);
     }
 
     /**
@@ -22,7 +31,12 @@ class StoreSurveyRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:1000',
+            'user_id' => 'exists:users,id',
+            'status' => 'required|boolean',
+            'description' => 'nullable|string',
+            'expire_date' => 'nullable|date|after:tomorrow',
+
         ];
     }
 }
